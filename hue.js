8063d03c34ee649dc847e93f19488f119ad7aa5b
Arthur <partylich@gmail.com>
2012-11-20 09:01:25 -0500

    Housekeeping

--- hue.js
+++ hue.js
@@ -7,12 +7,7 @@ define(function () {
    * @param {Number} a
    * @returns {Hue}
    */
-  //class Hue {
   function Hue(r, g, b, a) {
-  //int r, g, b, a;
-    //
-//      Hue([this.r = 0, this.g = 0, this.b = 0, this.a = 1]);
-    //}
     this.r = (r || 0);
     this.g = (g || 0);
     this.b = (b || 0);
